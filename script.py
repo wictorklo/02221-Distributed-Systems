ab6732587b8b9ca9a879b3b037951a2df66f661b
Barrikad <S184459@student.dtu.dk>
2022-05-09 13:17:39 +0200

    fixed message formats

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from pyparsing import nullDebugAction
 from drones import ADrone, BDrone
 from network_interfaces import NetworkInterface, Message
+import json
 
 
 class Script:
@@ -13,13 +14,27 @@ class Script:
         pass
 
 def succeed1(map, a, b):
-    a[0].networkInterface.sendMessage(Message('{"source": "'+a[0].networkInterface.ID+'", "destination": "'+b[0].networkInterface.ID+'", "payload":"1", "ttl":5}'))
+    message = Message()
+    message.data = {
+        "source" : a[0].networkInterface.ID,
+        "destination" : b[0].networkInterface.ID,
+        "ttl" : 5,
+        "payload" : json.dumps({"type" : "observation","tiles" : [], "drones" : []})
+    }
+    a[0].networkInterface.sendMessage(message)
     return map, a, b, True
 
 transmissionSucceed = [Script(succeed1)]
 
 def dropped1(map, a, b):
-    a[0].networkInterface.sendMessage(Message('{"source": "'+a[0].networkInterface.ID+'", "destination": "'+b[0].networkInterface.ID+'", "payload":"1"}'))
+    message = Message()
+    message.data = {
+        "source" : a[0].networkInterface.ID,
+        "destination" : b[0].networkInterface.ID,
+        "ttl" : 5,
+        "payload" : json.dumps({"type" : "observation","tiles" : [], "drones" : []})
+    }
+    a[0].networkInterface.sendMessage(message)
     a[0].networkInterface.getOutgoing()
     return map, a, b, True
 
